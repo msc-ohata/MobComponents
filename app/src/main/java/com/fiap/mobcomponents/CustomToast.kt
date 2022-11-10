983422c15a2342cf4c2fb9d90acede5150dc6373
Daniel Ohata <profdaniel.ohata@fiap.com.br>
2022-11-10 12:28:56 -0300

    Update

---
+++ app/src/main/java/com/fiap/mobcomponents/CustomToast.kt
@@ -0,0 +1,81 @@
+package com.fiap.mobcomponents
+
+import android.app.Activity
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+
+object CustomToast {
+
+    private fun showToast(
+        activity: Activity,
+        backgroundToast: Drawable?,
+        icon: Drawable?,
+        message: String,
+        duration: Int = Toast.LENGTH_SHORT
+    ) {
+        val toastLayout: View = activity.layoutInflater.inflate(R.layout.custom_toast, null)
+        var toast = Toast(activity)
+        toast.view = toastLayout
+        toast.view!!.background = backgroundToast
+        toastLayout.findViewById<TextView>(R.id.tvMessageToast).text = message
+        val ivIconToast = toastLayout.findViewById<ImageView>(R.id.ivIconToast)
+            if (icon == null){
+                ivIconToast.visibility = View.GONE
+                } else {
+                    ivIconToast.visibility = View.VISIBLE
+                    ivIconToast.setImageDrawable(icon)
+                }
+                toast.duration = duration
+                toast.show()
+        }
+
+    fun success(activity: Activity, message: String) {
+        showToast(
+            activity,
+            ContextCompat.getDrawable(activity, R.drawable.toast_bg_success),
+            ContextCompat.getDrawable(activity, R.drawable.ic_success_foreground),
+            message
+        )
+    }
+
+    fun warning(activity: Activity, message: String) {
+        showToast(
+            activity,
+            ContextCompat.getDrawable(activity, R.drawable.toast_bg_warning),
+            ContextCompat.getDrawable(activity, R.drawable.ic_warning_foreground),
+            message
+        )
+    }
+
+    fun error(activity: Activity, message: String) {
+        showToast(
+            activity,
+            ContextCompat.getDrawable(activity, R.drawable.toast_bg_error),
+            ContextCompat.getDrawable(activity, R.drawable.ic_error_foreground),
+            message
+        )
+    }
+
+    fun info(activity: Activity, message: String) {
+        showToast(
+            activity,
+            ContextCompat.getDrawable(activity, R.drawable.toast_bg_info),
+            ContextCompat.getDrawable(activity, R.drawable.ic_info_foreground),
+            message
+        )
+    }
+
+    fun default(activity: Activity, message: String) {
+        showToast(
+            activity,
+            ContextCompat.getDrawable(activity, R.drawable.toast_bg_default),
+            null,
+            message
+        )
+    }
+}
